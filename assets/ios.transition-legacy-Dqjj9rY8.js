80e768312b61a158e8af0db40e7fed0dcc0f0984
johnfrichmagan <johnfrichmagan530@gmail.com>
2025-02-11 23:02:43 +0800

    Updates

--- assets/ios.transition-legacy-Dqjj9rY8.js
+++ assets/ios.transition-legacy-Dqjj9rY8.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BxQuMpy3.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.i,n=t.j}],execute:function(){
+System.register(["./index-legacy-BITKxUlK.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
