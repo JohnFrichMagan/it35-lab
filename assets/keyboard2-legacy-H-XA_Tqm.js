41bab2c049c8a6f426cecf3bf76b26dd643d88ab
johnfrichmagan <johnfrichmagan530@gmail.com>
2025-02-17 16:11:09 +0800

    Updates

--- assets/keyboard2-legacy-H-XA_Tqm.js
+++ assets/keyboard2-legacy-H-XA_Tqm.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-nNOiHRd7.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
+System.register(["./index-legacy-BWC6BkoP.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/swipe-back-legacy-DKgXkCD4.js
+++ assets/swipe-back-legacy-DKgXkCD4.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-nNOiHRd7.js","./index3-legacy-BgnaNM3H.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
+System.register(["./index-legacy-BWC6BkoP.js","./index3-legacy-CGLyHTdT.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
