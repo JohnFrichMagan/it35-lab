75fdb6748c050ac8273ef908b9c8a6b20c1af820
johnfrichmagan <johnfrichmagan530@gmail.com>
2025-02-17 16:35:47 +0800

    Updates

--- assets/swipe-back-legacy-BX2vuU9g.js
+++ assets/swipe-back-legacy-BX2vuU9g.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BWC6BkoP.js","./index3-legacy-CGLyHTdT.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
+System.register(["./index-legacy-DukwT0sw.js","./index3-legacy-BAN6AewP.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
