078e9198cc484c1d6239eaace8a61c65b94d13aa
johnfrichmagan <johnfrichmagan530@gmail.com>
2025-03-10 14:01:10 +0800

    added files

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,77 +1,71 @@
 import { 
   IonAvatar,
-    IonButton,
-    IonButtons,
-      IonCol,
-      IonContent, 
-      IonGrid, 
-      IonHeader, 
-      IonInput, 
-      IonInputPasswordToggle, 
-      IonMenuButton, 
-      IonPage, 
-      IonRow, 
-      IonTitle, 
-      IonToolbar, 
-      useIonRouter
-  } from '@ionic/react';
+  IonButton,
+  IonCol,
+  IonContent, 
+  IonGrid, 
+  IonHeader, 
+  IonInput, 
+  IonInputPasswordToggle, 
+  IonPage, 
+  IonRow, 
+  IonTitle, 
+  IonToolbar, 
+  useIonRouter,
+  IonText
+} from '@ionic/react';
 
-  const Login: React.FC = () => {
-    const navigation = useIonRouter();
+const Login: React.FC = () => {
+  const navigation = useIonRouter();
 
-    const doLogin = () => {
-        navigation.push('/it35-lab/app','forward','replace');
-    }
-    return (
-      <IonPage>
-        <IonHeader>
-          <IonToolbar>
-            <IonTitle>Login</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <IonContent className='ion-padding'>
-        
-        <div
-          style={{
-            marginTop: '25%',
-          }}
-        >
-          <IonGrid style={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            height: '100%',
-          }}>
-            <IonRow >
-              
+  const doLogin = () => {
+    navigation.push('/it35-lab/app', 'forward', 'replace');
+  };
+
+  const goToRegister = () => {
+    navigation.push('/it35-lab/register');
+  };
+
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Login</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent className='ion-padding'>
+        <div style={{ marginTop: '25%' }}>
+          <IonGrid style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100%' }}>
+            <IonRow>
               <IonCol size="8">
-                <IonAvatar style={{
-                    display: 'flex',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                  }}>
-                  <img 
-                  style={{
-                    width:'100px',
-                    objectFit:'cover',
-                  }}
-                  alt="Silhouette of a person's head" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8IuuDjoVX1X4nogT6N9ZqKE9uoTYkU8eSzQ&s" />
+                <IonAvatar style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                  <img
+                    style={{ width: '100px', objectFit: 'cover' }}
+                    alt="Silhouette of a person's head"
+                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8IuuDjoVX1X4nogT6N9ZqKE9uoTYkU8eSzQ&s"
+                  />
                 </IonAvatar>
               </IonCol>
             </IonRow>
           </IonGrid>
-          <IonInput label="Username" >
+          <IonInput label="Username" placeholder="Enter username" />
+          <IonInput type="password" label="Password">
+            <IonInputPasswordToggle slot="end"></IonInputPasswordToggle>
           </IonInput>
-          <IonInput type="password" label="Password" value="gg">
-            <IonInputPasswordToggle  slot="end"></IonInputPasswordToggle>
-          </IonInput>
-          <IonButton onClick={() => doLogin()} expand="full">
+          <IonButton onClick={doLogin} expand="full">
             Login
           </IonButton>
+
+          <IonText color="primary" style={{ display: 'block', marginTop: '15px', textAlign: 'center' }}>
+            Don't have an account?{' '}
+            <span onClick={goToRegister} style={{ textDecoration: 'underline', cursor: 'pointer' }}>
+              Sign up
+            </span>
+          </IonText>
         </div>
-            
-        </IonContent>
-      </IonPage>
-    );
-  };
+      </IonContent>
+    </IonPage>
+  );
+};
+
 export default Login;
