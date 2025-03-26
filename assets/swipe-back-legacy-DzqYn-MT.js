5d972df8c8ab67a85b0cd824332ce8867cd92982
john <johnfrichmagan530@gmail.com>
2025-03-26 20:49:35 +0800

    Updates

--- assets/swipe-back-legacy-DzqYn-MT.js
+++ assets/swipe-back-legacy-DzqYn-MT.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BDCkyOkS.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-C5W3aS3L.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
