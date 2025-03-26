315e183cb4c99b18edc24d8e610a806c96427472
john <johnfrichmagan530@gmail.com>
2025-03-26 21:17:27 +0800

    Updated

--- src/pages/Home.tabs/Favorite.tsx
+++ src/pages/Home.tabs/Favorite.tsx
@@ -31,7 +31,7 @@ import {
                 <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
               </IonCardHeader>
         
-              <IonCardContent>Here's a small text description for the card content. Nothing more, nothing less.</IonCardContent>
+              <IonCardContent>Here's a small text description for the card content. Nothing more, nothing lessthan.</IonCardContent>
             </IonCard>
         </IonContent>
       </IonPage>
