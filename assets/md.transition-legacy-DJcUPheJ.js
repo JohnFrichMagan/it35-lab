b1062e9b86b415686002031cb0721250719c7c3b
johnmagan <johnfrichmagan530@gmail.com>
2025-04-07 14:06:58 +0800

    Updates

--- assets/md.transition-legacy-DJcUPheJ.js
+++ assets/md.transition-legacy-DJcUPheJ.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-vCEu_zqz.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-BAueqG_I.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
