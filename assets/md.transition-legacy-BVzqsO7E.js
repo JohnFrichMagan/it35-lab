6bb6cc338509b4d03f8541de5628d330927b623e
john <johnfrichmagan530@gmail.com>
2025-04-21 11:29:10 +0800

    Updates

--- assets/md.transition-legacy-BVzqsO7E.js
+++ assets/md.transition-legacy-BVzqsO7E.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-Dv6iaq-a.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-Cwxs0dPf.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
