f3d5508a549f1034fd1579df772fa73b3e5a5ab6
frich <johnfrichmagan530@gmail.com>
2025-04-28 17:46:44 +0800

    Updates

--- assets/ios.transition-legacy-DEuYIhlR.js
+++ assets/ios.transition-legacy-DEuYIhlR.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-TSbuNmnL.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-DNUiCNt_.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
